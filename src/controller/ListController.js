b1278cddf32726ba04c033fad0abc65bad53c5db
Carlos Henrique Alvarenga dos Santos <carlosho200@hotmail.com>
2022-01-10 13:57:12 -0400

    Updates search all Lists functionality

--- src/controller/ListController.js
+++ src/controller/ListController.js
@@ -49,15 +49,35 @@ class ListController {
 
     static async getAllLists(req, res) {
         
+        const t = await database.sequelize.transaction();
+
         try {
-            const lists = await database.List.findAll();
+            const lists = await database.List.findAll({transaction: t});
 
-            if (lists.length != 0)
-                return res.status(200).json(lists);
+            if (lists.length == 0) {
+                t.rollback();
+                return res.status(404).json({});
+            }
+            
+            let listsArray = [];
+            for (let l of lists) {                            
+                
+                const tasksFromList = await database.Task.findAll({
+                    where: {ListId: l.dataValues.id},
+                    transaction: t                    
+                });
+                
+                listsArray.push(
+                    ListController.createObjectStructureToPresentListWithTasks(l, tasksFromList)
+                );
 
-            return res.status(404).json({});
+            }
+
+            t.commit();
+            res.status(200).json(listsArray);
 
         } catch (error) {
+            t.rollback();
             return res.status(500).json(error);
         }
 
